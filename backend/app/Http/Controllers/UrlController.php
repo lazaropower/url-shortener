93ac3e921fbd902af5b2c564504a4445daa5a70b
Lázaro <20268108+lazaropower@users.noreply.github.com>
2024-03-05 12:35:33 +0200

    Add url resource.
    - Add url model.
    - Add database migration.
    - Add url controller and short-url endpoint.

---
+++ backend/app/Http/Controllers/UrlController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Url;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+
+class UrlController extends Controller
+{
+    public function shortenUrl(Request $request)
+    {
+        // Validate the request data
+        $validator = Validator::make($request->all(), [
+            'originalUrl' => 'required|url|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        // Retrieve the original url
+        $originalUrl = $request->input('originalUrl');
+
+        
+        // Check if the url exists already in the database so we do not generate the hash and save it
+        $url = Url::where('original_url', $originalUrl)->first();
+
+        if ($url) {
+            return response()->json(['hash' => $url->hash], 200);
+        }
+
+        // Generate hash
+        $hash = $this->generateHash($originalUrl);
+
+        // Save it in the database if does not exist yet
+        Url::create([
+            'original_url' => $originalUrl,
+            'hash'=> $hash
+        ]);
+
+        return response()->json(['hash' => $hash], 200);
+    }
+
+    private function generateHash(string $url): string
+    {
+        $hashedUrl = md5($url);
+        $result = substr($hashedUrl, 0, 6);
+
+        return $result;
+    }
+}
